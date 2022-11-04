6e8419d5e32ffabdd282404925f5903523b5fe82
toby <leutoby@gmail.com>
2022-11-04 07:29:26 +0100

    rewrite db query

--- utils/cronjob.js
+++ utils/cronjob.js
@@ -1,4 +1,5 @@
 const cron = require("node-cron");
+const moment = require("moment");
 const Contact = require("../models/Contact");
 const logger = require("./logger");
 const formatDate = require("./dateFormatter");
@@ -9,11 +10,15 @@ function cronjob() {
     logger.info("running cron job");
 
     const day = new Date();
+    const today = moment().startOf("day");
     const compiledDate = formatDate(day);
     const filename = `${compiledDate}.vcf`;
 
     const dbContacts = await Contact.find({
-      joined: { $gt: new Date(day - 1 * 60 * 60 * 24 * 1000) },
+      joined: {
+        $gte: today.toDate(),
+        $lte: moment(today).endOf("day").toDate(),
+      },
     });
 
     if (dbContacts.length) {
